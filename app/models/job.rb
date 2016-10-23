5be20d2658a95568f156b20777b030b0070cf3ed
Ali Schlereth <aschlere@sbcglobal.net>
2016-10-22 18:31:39 -0600

    Begin Analysis Methods

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,12 @@ class Job < ActiveRecord::Base
   belongs_to :company
   belongs_to :category
   has_many :comments
+
+
+  def self.sort_by_interest
+      jobs = Job.all.group(:level_of_interest)
+      byebug
+  end
+
+
 end
